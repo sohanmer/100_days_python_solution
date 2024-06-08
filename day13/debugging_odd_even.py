5fbb10098d8d9eaeba4edc5548c0738dd561f1b4
standards1510 <standards1510@gmail.com>
2024-06-08 07:08:08 +0530

    day13

---
+++ day13/debugging_odd_even.py
@@ -0,0 +1,7 @@
+number = int(input()) # Which number do you want to check?
+
+if number % 2 == 0: # Error was in this line. We need to 2 "=" sign while comparing
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+  
