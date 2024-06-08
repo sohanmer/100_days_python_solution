2778794788ee59af9bb23a03571e677de0a15bef
standards1510 <standards1510@gmail.com>
2024-06-08 07:13:25 +0530

    day13

---
+++ day13/debugging_leap_year.py
@@ -0,0 +1,13 @@
+# Which year do you want to check?
+year = int(input()) # use type conversion to convert to int
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
