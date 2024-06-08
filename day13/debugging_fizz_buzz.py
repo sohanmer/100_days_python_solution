a0011c65d90997f46ca2a8411538586fbc1b3862
standards1510 <standards1510@gmail.com>
2024-06-08 07:11:39 +0530

    day13

--- day13/debugging_fizz_buzz.py
+++ day13/debugging_fizz_buzz.py
@@ -1,10 +1,10 @@
 target = int(input())
 for number in range(1, target + 1):
-  if number % 3 == 0 and number % 5 == 0:
+  if number % 3 == 0 and number % 5 == 0: # use "and" instead of "or"
     print("FizzBuzz")
-  elif number % 3 == 0:
+  elif number % 3 == 0: # use elif instead of if
     print("Fizz")
-  elif number % 5 == 0:
+  elif number % 5 == 0: # use elif instead of if
     print("Buzz")
   else:
-    print(number)
+    print(number) # remove [] brackets
--- day13/debugging_odd_even.py
+++ day13/debugging_odd_even.py
@@ -1,6 +1,6 @@
 number = int(input()) # Which number do you want to check?
 
-if number % 2 == 0: # Error was in this line. We need to 2 "=" sign while comparing
+if number % 2 == 0: # use "==" instead of "="
   print("This is an even number.")
 else:
   print("This is an odd number.")
