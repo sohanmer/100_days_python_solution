46bc4c337591a0d8e8168ac35c0a5ba379d49a7e
standards1510 <standards1510@gmail.com>
2024-06-25 21:30:19 +0530

    day17: completed

---
+++ day17/quiz/main.py
@@ -0,0 +1,15 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_bank.append(Question(question['text'], question['answer']))
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You've completed the quiz!")
+print(f"Your final score is: {quiz.score}/{len(question_bank)}")
