a61a5ef0b28f2a079720e8b115899c36ddfef64e
standards1510 <standards1510@gmail.com>
2024-06-30 14:59:21 +0530

    day23: final project

---
+++ day23/turtle-crossing-game/main.py
@@ -0,0 +1,38 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+cars = []
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.listen()
+    car_manager.create_car()
+    car_manager.move_cars()
+    screen.onkey(player.move_forward, "Up")
+    screen.update()
+
+    # Detect collision with upper wall
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.update_scoreboard()
+
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+
+screen.exitonclick()
